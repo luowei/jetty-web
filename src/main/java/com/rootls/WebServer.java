f5bf3bea85ccb2eb6bfe483ede57884bf97d3239
luowei <luowei505050@126.com>
2013-10-18 16:17:58 +0800

    first commit

---
+++ src/main/java/com/rootls/WebServer.java
@@ -0,0 +1,170 @@
+package com.rootls;
+
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.NCSARequestLog;
+import org.eclipse.jetty.server.RequestLog;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.RequestLogHandler;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.eclipse.jetty.util.thread.ThreadPool;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Example WebServer class which sets up an embedded Jetty appropriately
+ * whether running in an IDE or in "production" mode in a shaded jar.
+ * 
+ * @author steve liles
+ */
+public class WebServer
+{
+	// TODO: You should configure this appropriately for your environment
+	private static final String LOG_PATH = "./var/logs/access/yyyy_mm_dd.request.log";
+	
+	private static final String WEB_XML = "META-INF/webapp/WEB-INF/web.xml";
+    private static final String CLASS_ONLY_AVAILABLE_IN_IDE = "com.rootls.IDE";
+    private static final String PROJECT_RELATIVE_PATH_TO_WEBAPP = "src/main/java/META-INF/webapp";
+    
+    public static interface WebContext
+    {
+        public File getWarPath();
+        public String getContextPath();
+    }
+    
+    
+    private Server server;
+    private int port;
+    private String bindInterface;
+    
+    public WebServer(int aPort)
+    {
+        this(aPort, null);
+    }
+    
+    public WebServer(int aPort, String aBindInterface)
+    {        
+        port = aPort;
+        bindInterface = aBindInterface;
+    }
+    
+    public void start() throws Exception
+    {
+        server = new Server();
+
+        server.setThreadPool(createThreadPool());
+        server.addConnector(createConnector());
+        server.setHandler(createHandlers());        
+        server.setStopAtShutdown(true);
+                
+        server.start();       
+    }
+    
+    public void join() throws InterruptedException
+    {
+        server.join();
+    }
+    
+    public void stop() throws Exception
+    {        
+        server.stop();
+    }
+    
+    private ThreadPool createThreadPool()
+    {
+    	// for your environment - this is an example only
+        QueuedThreadPool _threadPool = new QueuedThreadPool();
+        _threadPool.setMinThreads(10);
+        _threadPool.setMaxThreads(100);
+        return _threadPool;
+    }
+    
+    private SelectChannelConnector createConnector()
+    {
+        SelectChannelConnector _connector = new SelectChannelConnector();
+        _connector.setPort(port);
+        _connector.setHost(bindInterface);
+        return _connector;
+    }
+    
+    private HandlerCollection createHandlers()
+    {                
+        WebAppContext _ctx = new WebAppContext();
+        _ctx.setContextPath("/");
+        
+        if(isRunningInShadedJar())
+        {          
+            _ctx.setWar(getShadedWarUrl());
+        }
+        else
+        {            
+            _ctx.setWar(PROJECT_RELATIVE_PATH_TO_WEBAPP);
+        }
+        
+        List<Handler> _handlers = new ArrayList<Handler>();
+        
+        _handlers.add(_ctx);
+        
+        HandlerList _contexts = new HandlerList();
+        _contexts.setHandlers(_handlers.toArray(new Handler[0]));
+        
+        RequestLogHandler _log = new RequestLogHandler();
+        _log.setRequestLog(createRequestLog());
+        
+        HandlerCollection _result = new HandlerCollection();
+        _result.setHandlers(new Handler[] {_contexts, _log});
+        
+        return _result;
+    }
+    
+    private RequestLog createRequestLog()
+    {
+        NCSARequestLog _log = new NCSARequestLog();
+        
+    	File _logPath = new File(LOG_PATH);
+        _logPath.getParentFile().mkdirs();
+                
+        _log.setFilename(_logPath.getPath());
+        _log.setRetainDays(90);
+        _log.setExtended(false);
+        _log.setAppend(true);
+        _log.setLogTimeZone("GMT");
+        _log.setLogLatency(true);
+        return _log;
+    }
+    
+//---------------------------
+// Discover the war path
+//---------------------------   
+    
+    private boolean isRunningInShadedJar()
+    {
+        try
+        {
+            Class.forName(CLASS_ONLY_AVAILABLE_IN_IDE);
+            return false;
+        }
+        catch(ClassNotFoundException anExc)
+        {
+            return true;
+        }
+    }
+    
+    private URL getResource(String aResource)
+    {
+        return Thread.currentThread().getContextClassLoader().getResource(aResource); 
+    }
+    
+    private String getShadedWarUrl()
+    {
+        String _urlStr = getResource(WEB_XML).toString();
+        // Strip off "WEB-INF/web.xml"
+        return _urlStr.substring(0, _urlStr.length() - 15);
+    }
+}
---
+++ src/main/java/com/rootls/web/Home.java
@@ -0,0 +1,16 @@
+package com.rootls.web;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.*;
+
+@Controller
+public class Home {
+
+	@RequestMapping("/")
+	public ModelAndView home()
+	{
+		return new ModelAndView("index");
+	}
+	
+}
